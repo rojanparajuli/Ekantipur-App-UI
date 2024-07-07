b23f866907c0e5b1f0daeb8422b3e1e5a749c384
Rojan Parajuli <rojanparajuli13@gmail.com>
2024-07-07 12:23:00 +0545

    profile complete

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class HomeController extends GetxController {
+  var selectedIndex = 0.obs;
+  final pageController = PageController();
+
+
+
+  void onItemTapped(int index) {
+    selectedIndex.value = index;
+    pageController.jumpToPage(index);
+  }
+
+  void onPageChanged(int index) {
+    selectedIndex.value = index;
+  }
+}
