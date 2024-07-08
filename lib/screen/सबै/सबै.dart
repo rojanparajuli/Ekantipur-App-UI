0bb5b39465ed92f43ad114371a907d9164fbbaa8
Rojan Parajuli <rojanparajuli13@gmail.com>
2024-07-08 10:37:23 +0545

    tabbar

--- lib/screen/सबै/सबै.dart
+++ lib/screen/सबै/सबै.dart
@@ -8,10 +8,38 @@ class AllNewsScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Row(children: <Widget>[
-            
-          ],)
+          Container(
+            color: Colors.blue, 
+            padding: const EdgeInsets.symmetric(vertical: 16.0), 
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround, 
+              children: <Widget>[
+                _buildNavBarItem('Home'),
+                _buildNavBarItem('World'),
+                _buildNavBarItem('Politics'),
+                _buildNavBarItem('Business'),
+                _buildNavBarItem('Technology'),
+                _buildNavBarItem('Science'),
+                _buildNavBarItem('Health'),
+                _buildNavBarItem('Sports'),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildNavBarItem(String text) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white), 
+        borderRadius: BorderRadius.circular(4.0), 
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(color: Colors.white), 
       ),
     );
   }
